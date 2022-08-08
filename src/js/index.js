5f8f777a2871b0cf849ca680088b7f945053cb42
meeyoungchoi <goodlife1359@gmail.com>
2022-08-08 21:59:45 +0900

    확인 버튼 클릭시 메뉴추가 이벤트 처리 및 확인버튼 클릭에 대한 예외처리

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,50 @@ function App() {
         e.preventDefault();
       });
       
+      const addMenuName = () => {
+       // 값을 입력하지 않고 엔터를 누른경우 예외처리
+       if ($("#espresso-menu-name").value === "") {
+        alert("값을 입력해주세요.");
+        return;
+      }
+
+      if (e.key === "Enter") {
+       const espressoMenuName = $("#espresso-menu-name").value;    
+       const menuItemTemplate = (espressoMenuName) => `<li class="menu-list-item d-flex items-center py-2">
+       <span class="w-100 pl-2 menu-name">${name}</span>
+       <button
+         type="button"
+         class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+       >
+         수정
+       </button>
+       <button
+         type="button"
+         class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+       >
+         삭제
+       </button>
+      </li>`;
+      $("#espresso-menu-list").insertDjacentHTML(
+        "beforeend", 
+        menuItemTemplate(espressoMenuName)
+      );
+
+      const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+      $(".menu-count").innerText = `총 ${menuCount} 개`;
+      $("#espresso-menu-name").value = ""; 
+      };
+
+
+      }
+
+    $("#espresso-menu-submiit-button").addEventListener("click", () => {
+      addMenuName();
+    })  
+
+
+     
+
     // 메뉴 이름 입력받기  
     document
       $("#espresso-menu-name")
@@ -17,38 +61,8 @@ function App() {
         if (e.key !== "Enter") {
             return;
         }
-
-        // 값을 입력하지 않고 엔터를 누른경우 예외처리
-        if ($("#espresso-menu-name").value === "") {
-            alert("값을 입력해주세요.");
-            return;
-        }
-
-        if (e.key === "Enter") {
-           const espressoMenuName = $("#espresso-menu-name").value;    
-           const menuItemTemplate = (espressoMenuName) => `<li class="menu-list-item d-flex items-center py-2">
-           <span class="w-100 pl-2 menu-name">${name}</span>
-           <button
-             type="button"
-             class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-           >
-             수정
-           </button>
-           <button
-             type="button"
-             class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-           >
-             삭제
-           </button>
-         </li>`;
-        };
-        $("#espresso-menu-list").insertDjacentHTML(
-            "beforeend", 
-            menuItemTemplate(espressoMenuName)
-        );
-
-        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-        $(".menu-count").innerText = `총 ${menuCount} 개`;
-        $("#espresso-menu-name").value = ""; 
+        addMenuName();
+        
+       
       });
 }
